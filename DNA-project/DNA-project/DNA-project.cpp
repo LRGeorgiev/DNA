d36dbfcd196a4bd312ae7acb33b88f7205e0880d
LRGeorgiev <lrgeorgiev20@codingburgas.bg>
2023-03-21 21:28:20 +0200

    Add raylib library

--- DNA-project/DNA-project/DNA-project.cpp
+++ DNA-project/DNA-project/DNA-project.cpp
@@ -1,20 +1,145 @@
-// DNA-project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
 
-int main()
+#include "raylib.h"
+
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+   
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+
+    GameScreen currentScreen = LOGO;
+
+   
+
+    int framesCounter = 0;          
+    
+    SetTargetFPS(60);               
+  
+    // Main game loop
+    while (!WindowShouldClose())    
+    {
+        
+        
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            // TODO: Update LOGO screen variables here!
+            
+            Texture2D texture = LoadTexture("resources/logo.png");
+            
+
+         
+
+            framesCounter++;    // Count frames
+
+            // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+            
+                BeginDrawing();
+
+                ClearBackground(RAYWHITE);
+
+                DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2, WHITE);
+
+                DrawText("this IS a texture!", 360, 370, 10, GRAY);
+
+                EndDrawing();
+                currentScreen = TITLE;
+            
+        } break;
+        case TITLE:
+        {
+            // TODO: Update TITLE screen variables here!
+
+            // Press enter to change to GAMEPLAY screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = GAMEPLAY;
+            }
+        } break;
+        case GAMEPLAY:
+        {
+            // TODO: Update GAMEPLAY screen variables here!
+
+            // Press enter to change to ENDING screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = ENDING;
+            }
+        } break;
+        case ENDING:
+        {
+            // TODO: Update ENDING screen variables here!
+
+            // Press enter to return to TITLE screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = TITLE;
+            }
+        } break;
+        default: break;
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            // TODO: Draw LOGO screen here!
+            DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+            DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
+
+        } break;
+        case TITLE:
+        {
+            // TODO: Draw TITLE screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+            DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+            DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+        } break;
+        case GAMEPLAY:
+        {
+            // TODO: Draw GAMEPLAY screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+            DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
+            DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+
+        } break;
+        case ENDING:
+        {
+            // TODO: Draw ENDING screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+            DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+            DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+
+        } break;
+        default: break;
+        }
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+
+    // TODO: Unload all loaded data (textures, fonts, audio) here!
+
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
