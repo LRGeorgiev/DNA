6fc816ce804a18d942a40262fed062973933659a
LRGeorgiev <lrgeorgiev20@codingburgas.bg>
2023-04-03 12:39:11 +0300

    Add showTitleScreen function and an icon

--- DNA-project/DNA-project/DNA-project.cpp
+++ DNA-project/DNA-project/DNA-project.cpp
@@ -1,17 +1,26 @@
-
-
-
 #include "Main.h"
 
+bool showTitleScreen() {
+    bool startGame = false;
+    while (!startGame && !WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
+        DrawText("Biology quiz", screenWidth / 2 - MeasureText("Biology quiz", 40) / 2, screenHeight / 3, 40, BLACK);
+        DrawText("Press ENTER to start the game", screenWidth / 2 - MeasureText("Press ENTER to start the game", 20) / 2, screenHeight / 2, 20, GREEN);
 
+        if (IsKeyPressed(KEY_ENTER)) {
+            startGame = true;
+        }
 
+        EndDrawing();
+    }
 
-
-
+    return startGame;
+}
 
 int main() {
-    InitWindow(screenWidth, screenHeight, "Levels and Questions");
+    InitWindow(screenWidth, screenHeight, "Biology quiz");
 
     SetTargetFPS(60);
 
@@ -23,63 +32,63 @@ int main() {
     int currentLevelIndex = -1;
     int currentScore = 0;
 
-    while (!WindowShouldClose()) {
-        if (!isQuestionDisplayed) {
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
+    if (showTitleScreen()) {
+        while (!WindowShouldClose()) {
+            if (!isQuestionDisplayed) {
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
 
-            drawLevels();
+                drawLevels();
 
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                for (int i = 0; i < levelsCount; i++) {
-                    if (CheckCollisionPointRec(GetMousePosition(), levels[i].rect)) {
-                        currentLevelIndex = i;
-                        currentQuestionIndex = getRandomQuestionIndex();
-                        isQuestionDisplayed = true;
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    for (int i = 0; i < levelsCount; i++) {
+                        if (CheckCollisionPointRec(GetMousePosition(), levels[i].rect)) {
+                            currentLevelIndex = i;
+                            currentQuestionIndex = getRandomQuestionIndex();
+                            isQuestionDisplayed = true;
+                        }
                     }
                 }
 
+                EndDrawing();
             }
-
-
-            EndDrawing();
-        }
-        else {
-            BeginDrawing();
-            drawQuestion(questions[currentQuestionIndex]);
-            if (IsKeyPressed(KEY_ONE)) {
-                if (checkAnswer(questions[currentQuestionIndex], 1)) {
-                    currentScore++;
-                    levels[currentLevelIndex].rect.width = 0;
-                    levels[currentLevelIndex].rect.height = 0;
+            else {
+                BeginDrawing();
+                drawQuestion(questions[currentQuestionIndex]);
+                if (IsKeyPressed(KEY_ONE)) {
+                    if (checkAnswer(questions[currentQuestionIndex], 1)) {
+                        currentScore++;
+                        levels[currentLevelIndex].rect.width = 0;
+                        levels[currentLevelIndex].rect.height = 0;
+                    }
+                    isQuestionDisplayed = false;
                 }
-                isQuestionDisplayed = false;
-            }
-            else if (IsKeyPressed(KEY_TWO)) {
-                if (checkAnswer(questions[currentQuestionIndex], 2)) {
-                    currentScore++;
-                    levels[currentLevelIndex].rect.width = 0;
-                    levels[currentLevelIndex].rect.height = 0;
+                else if (IsKeyPressed(KEY_TWO)) {
+                    if (checkAnswer(questions[currentQuestionIndex], 2)) {
+                        currentScore++;
+                        levels[currentLevelIndex].rect.width = 0;
+                        levels[currentLevelIndex].rect.height = 0;
+                    }
+                    isQuestionDisplayed = false;
                 }
-                isQuestionDisplayed = false;
-            }
-            else if (IsKeyPressed(KEY_THREE)) {
-                if (checkAnswer(questions[currentQuestionIndex], 3)) {
-                    currentScore++;
-                    levels[currentLevelIndex].rect.width = 0;
-                    levels[currentLevelIndex].rect.height = 0;
+                else if (IsKeyPressed(KEY_THREE)) {
+                    if (checkAnswer(questions[currentQuestionIndex], 3)) {
+                        currentScore++;
+                        levels[currentLevelIndex].rect.width = 0;
+                        levels[currentLevelIndex].rect.height = 0;
+                    }
+                    isQuestionDisplayed = false;
                 }
-                isQuestionDisplayed = false;
-            }
-            else if (IsKeyPressed(KEY_FOUR)) {
-                if (checkAnswer(questions[currentQuestionIndex], 4)) {
-                    currentScore++;
-                    levels[currentLevelIndex].rect.width = 0;
-                    levels[currentLevelIndex].rect.height = 0;
+                else if (IsKeyPressed(KEY_FOUR)) {
+                    if (checkAnswer(questions[currentQuestionIndex], 4)) {
+                        currentScore++;
+                        levels[currentLevelIndex].rect.width = 0;
+                        levels[currentLevelIndex].rect.height = 0;
+                    }
+                    isQuestionDisplayed = false;
                 }
-                isQuestionDisplayed = false;
+                EndDrawing();
             }
-            EndDrawing();
         }
     }
 
--- DNA-project/DNA-project/Main.h
+++ DNA-project/DNA-project/Main.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "raylib.h"
+#include "C:\raylib\raylib\out\build\x64-Debug\raylib\include\raylib.h"
 #include <iostream>
 #include <string>
 #include <vector>
